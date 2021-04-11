645d7d4be62e7b52a57e654859a256476f42b998
Robbe Van Herck <robbe@robbevanherck.be>
2021-04-11 17:55:55 +0200

    More Frameworking

--- planet.py
+++ planet.py
@@ -6,12 +6,22 @@ class Planet:
     """
     Planet: represents an owned planet
     """
-    def handle_turn(self, _state) -> list[dict]:
+    def handle_turn(self, _state):
         """
         handle_turn: process the new turn
         """
-        return []
 
+    def get_moves(self) -> list:
+        """
+        get_moves: return the moves this planet intends to take
+        """
+
+    def get_distress(self) -> list[tuple[int, int]]:
+        """
+        get_distress: returns the times this planet will go into distress and
+        the amount of ships it needs to survive
+        """
+    
     def get_future(self):
         """
         get_future: getter for the future states of this planet
@@ -33,6 +43,17 @@ class Planet:
     def execute_distress_call(self, call_handles: list) -> (bool, list):
         """
         execute_distress_call: execute all distress call handling
+        """
+
+    def query_attack_call(self, call_handles: list) -> (bool, list):
+        """
+        query_attack_call: ask the planet how it can handle the attack calls
+        returning (has_changed, new_call_handles)
+        """
+
+    def execute_attack_call(self, call_handles: list) -> (bool, list):
+        """
+        execute_attack_call: execute all attack call handling
         """
 
     def make_feeder(self, other_planet: Planet):
